008331d31edead1465cb001cd9f11b4400c22b37
Huai328 <kaqhahk@yahoo.com.tw>
2026-07-18 19:01:15 +0800

    Update import_data.py

--- scripts/import_data.py
+++ scripts/import_data.py
@@ -220,8 +220,11 @@ def main():
         print(f'資料目錄: {os.path.abspath(DATA_DIR)}')
         print()
 
-        db.drop_all()
+        db.session.execute(db.text("DROP SCHEMA public CASCADE;"))
+        db.session.execute(db.text("CREATE SCHEMA public;"))
+        db.session.commit()
         db.create_all()
+
 
 
         import_countries()
